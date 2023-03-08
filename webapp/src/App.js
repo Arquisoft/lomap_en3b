50cc6e9e0716f667ea9c302334dd596f1e687be5
Sara Fernández Arias <uo269546@uniovi.es>
2023-03-08 21:45:32 +0100

    Login button redirects to Inrupt dialogue

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,45 +1,23 @@
 
 import React from 'react';
-import beachimg from './images/beach.jpg'; // Tell Webpack this JS file uses this image
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Login from "./Login/Login.js";
 
-const ourApp= {
-    name: 'LoMap',
-    group:'3B',
-    lab:'ASW-En-L.3',
-    groupMembers:['Manu','Juan','Sebastian','Sara'],
 
-};
 
-export function App() {
-  return (
-      <div className="intro">
-        <h1>Welcome to {ourApp.name}!</h1>
-
-        <p className="summary">
-          This is our first mock in react!:)
-        </p>
-      </div>
-  );
-}
-
-export default function First(){
+ function App(){
     return (
-            <section>
-            <h2>{ourApp.name} Info</h2>
-<p>        <img
-                // Import  the actual jpg
-                src= {beachimg} //equivalent to writing src={require('../public/beach.jpg'}
-                alt="A place to pin in lomap"
-            />
-</p>
-                <h3>Group members</h3>
-            <ul>
-                <li>{ourApp.groupMembers[0]}</li>
-                <li>{ourApp.groupMembers[1]}</li>
-                <li>{ourApp.groupMembers[2]}</li>
-                <li>{ourApp.groupMembers[3]}</li>
-            </ul>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />}/>
+            </Routes>
 
-            </section>
+        </BrowserRouter>
+
     );
 }
+export default App;
