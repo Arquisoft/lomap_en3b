5314386b72d7ea7e54065b31b9312ed3d09ca28c
Sara Fernández Arias <uo269546@uniovi.es>
2023-03-08 21:47:58 +0100

    Revert "Login button redirects to Inrupt dialogue"
    This reverts commit f6ffb4a7214f0492a24e12da08576bebdf8094b4.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,23 +1,45 @@
 
 import React from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
-import Login from "./Login/Login.js";
+import beachimg from './images/beach.jpg'; // Tell Webpack this JS file uses this image
 
+const ourApp= {
+    name: 'LoMap',
+    group:'3B',
+    lab:'ASW-En-L.3',
+    groupMembers:['Manu','Juan','Sebastian','Sara'],
 
+};
 
- function App(){
+export function App() {
+  return (
+      <div className="intro">
+        <h1>Welcome to {ourApp.name}!</h1>
+
+        <p className="summary">
+          This is our first mock in react!:)
+        </p>
+      </div>
+  );
+}
+
+export default function First(){
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />}/>
-            </Routes>
+            <section>
+            <h2>{ourApp.name} Info</h2>
+<p>        <img
+                // Import  the actual jpg
+                src= {beachimg} //equivalent to writing src={require('../public/beach.jpg'}
+                alt="A place to pin in lomap"
+            />
+</p>
+                <h3>Group members</h3>
+            <ul>
+                <li>{ourApp.groupMembers[0]}</li>
+                <li>{ourApp.groupMembers[1]}</li>
+                <li>{ourApp.groupMembers[2]}</li>
+                <li>{ourApp.groupMembers[3]}</li>
+            </ul>
 
-        </BrowserRouter>
-
+            </section>
     );
 }
-export default App;
