506e9ed01af9c62b848d3701d6196f6be68f6af4
Sara Fernández Arias <uo269546@uniovi.es>
2023-03-15 17:55:58 +0100

    final easthetics for prototype

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,22 +1,10 @@
-import './App.css';
+import './styles/App.css';
 import { SessionProvider} from "@inrupt/solid-ui-react";
 import { useState} from "react";
 import LoginForm from "./components/LoginForm"
 import { useSession } from "@inrupt/solid-ui-react/dist";
 
 import AuthenticatedUserView from "./components/UserView";
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom"
-
-
-import{GoogleMap, useLoadScript,Marker, InfoWindow} from "@react-google-maps/api";
-
-import Header from "./components/Header"; // Tell Webpack this JS file uses this image
-import {formatRelative} from "date-fns";
-import mapStyles from "./mapStyles";
 
 
 export default function App()
