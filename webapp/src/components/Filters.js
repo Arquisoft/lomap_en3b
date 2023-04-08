8f17cadc12ac4a075863a3f98bb7bb280b736049
Sara Fernández Arias <uo269546@uniovi.es>
2023-04-08 20:59:46 +0200

    Work on filters so far

--- webapp/src/components/Filters.js
+++ webapp/src/components/Filters.js
@@ -4,7 +4,7 @@ import {
     Grid,
     FormControlLabel,
     Checkbox,
-    Drawer, List, Box, Typography,  IconButton
+    Drawer, List, Box, Typography, IconButton, Button
 
 } from "@mui/material";
 import {FormControl} from "./styles/ListStyle";
@@ -16,35 +16,61 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import {Group} from "@mui/icons-material";
 
-export default function FilterSidebar(isVisible){
-    const [type, setType]=useState('');
-    const [visible, setVisible]=useState(isVisible);
-    const handleAddButtonClick = () => {
-
-    };
+export default function FilterSidebar(visible){
+    const [locationCategory, setLocationCategory]=useState([]);
 
     const style = {
         display: visible ? 'flex' : 'none',
-        flexDirection:'column'
+        flexDirection:'column',
+        bottom: '20vh',//I overwrite the inherited value because i want it to be smaller.
+
     };
+
+    /**
+     * I created this method to avoid creating a new state for each checkbox, so if new filters are included it's easier and readable.
+     *
+     * This method adds a value to the filter list if the checkbox has been checked, and deletes it in case it was unchecked.
+     * @param selected
+     * @param value
+     */
+    function updateFiltersSelected(selected, value){
+
+        let currentList=locationCategory;
+        let updated=currentList;
+
+        if(!selected) {
+
+             updated=currentList.filter(element=>element!=value);
+
+        }else{
+           updated.push(value);
+
+        }
+
+        setLocationCategory(updated);
+
+    }
 
     return (
        <Container style={style}>
            <Typography variant ="h3" > Filter pins!</Typography>
 
-           <FormControl style={{display:'flex',flexDirection:'column',flex:1}} value={type} onChange={(category)=>category.target.value}>
-
+           <FormControl style={{display:'flex',flexDirection:'column',flex:1}} value={locationCategory}  >
                <InputLabel>Location type</InputLabel>
 
-                     <FormControlLabel control={<Checkbox/>} style={{flex: 1}} label="Sight" value="Sight" />
-                    <FormControlLabel  control={<Checkbox/>} style={{flex: 1}} label="Restaurant"  value="Restaurant"/>
-                    <FormControlLabel  control={<Checkbox/>} style={{flex: 1}} label="Monument" value="Monument"/>
-                    <FormControlLabel control={<Checkbox/>} style={{flex: 1}} label="Park" value="Park" />
-                    <FormControlLabel control={<Checkbox/>} style={{flex: 1}}  label="Bar" value="Bar" />
-                    <FormControlLabel control={<Checkbox/>} style={{flex: 1}}  label="Shop" value="Shop"/>
+                     <FormControlLabel control={<Checkbox value="Sight" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}} label="Sight"  />
+                    <FormControlLabel  control={<Checkbox  value="Restaurant"  onChange={(e)=>updateFiltersSelected(e.target.checked, e.target.value)}/> }style={{flex: 1}} label="Restaurant" />
+                    <FormControlLabel  control={<Checkbox value="Monument" onChange={(e)=>updateFiltersSelected(e.target.checked, e.target.value )}/> } style={{flex: 1}} label="Monument" />
+                    <FormControlLabel control={<Checkbox  value="Park" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}} label="Park"  />
+                    <FormControlLabel control={<Checkbox  value="Bar"  onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}}  label="Bar" />
+                    <FormControlLabel control={<Checkbox  value="Shop" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> }style={{flex: 1}}  label="Shop"/>
 
 
            </FormControl>
+
+           <Button>
+               Clear filters
+           </Button>
 
        </Container>
 
