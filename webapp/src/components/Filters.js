80c23a226f6a3b80ef36018016f8f675bedb0aa6
Sara Fernández Arias <uo269546@uniovi.es>
2023-04-08 22:42:42 +0200

    attempt to make a clear filter

--- webapp/src/components/Filters.js
+++ webapp/src/components/Filters.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React,{useState} from 'react';
 
 import {
     Grid,
@@ -16,9 +16,10 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import {Group} from "@mui/icons-material";
 
+
 export default function FilterSidebar(visible){
     const [locationCategory, setLocationCategory]=useState([]);
-
+    const filterRef=React.createRef();
     const style = {
         display: visible ? 'flex' : 'none',
         flexDirection:'column',
@@ -39,14 +40,10 @@ export default function FilterSidebar(visible){
         let updated=currentList;
 
         if(!selected) {
-
              updated=currentList.filter(element=>element!=value);
-
         }else{
            updated.push(value);
-
         }
-
         setLocationCategory(updated);
 
     }
@@ -54,21 +51,18 @@ export default function FilterSidebar(visible){
     return (
        <Container style={style}>
            <Typography variant ="h3" > Filter pins!</Typography>
-
            <FormControl style={{display:'flex',flexDirection:'column',flex:1}} value={locationCategory}  >
                <InputLabel>Location type</InputLabel>
 
-                     <FormControlLabel control={<Checkbox value="Sight" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}} label="Sight"  />
-                    <FormControlLabel  control={<Checkbox  value="Restaurant"  onChange={(e)=>updateFiltersSelected(e.target.checked, e.target.value)}/> }style={{flex: 1}} label="Restaurant" />
-                    <FormControlLabel  control={<Checkbox value="Monument" onChange={(e)=>updateFiltersSelected(e.target.checked, e.target.value )}/> } style={{flex: 1}} label="Monument" />
-                    <FormControlLabel control={<Checkbox  value="Park" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}} label="Park"  />
-                    <FormControlLabel control={<Checkbox  value="Bar"  onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}}  label="Bar" />
-                    <FormControlLabel control={<Checkbox  value="Shop" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> }style={{flex: 1}}  label="Shop"/>
-
-
+               <FormControlLabel control={<Checkbox ref={filterRef} value="Sight" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}} label="Sight"  />
+               <FormControlLabel  control={<Checkbox ref={filterRef} value="Restaurant"  onChange={(e)=>updateFiltersSelected(e.target.checked, e.target.value)}/> }style={{flex: 1}} label="Restaurant" />
+               <FormControlLabel  control={<Checkbox ref={filterRef} value="Monument" onChange={(e)=>updateFiltersSelected(e.target.checked, e.target.value )}/> } style={{flex: 1}} label="Monument" />
+               <FormControlLabel control={<Checkbox  ref={filterRef} value="Park" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}} label="Park"  />
+               <FormControlLabel control={<Checkbox ref ={filterRef} value="Bar"  onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> } style={{flex: 1}}  label="Bar" />
+               <FormControlLabel control={<Checkbox  ref={filterRef} value="Shop" onChange={(e)=>updateFiltersSelected(e.target.checked,e.target.value )}/> }style={{flex: 1}}  label="Shop"/>
            </FormControl>
 
-           <Button>
+           <Button onClick={()=>  console.log(filterRef)}>
                Clear filters
            </Button>
 
