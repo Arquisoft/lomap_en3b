a38464e4504f2b30f4316593d2f1998df3b07730
Juan González <juan@MacBook-Pro-de-Juan.local>
2023-04-13 03:34:47 +0200

    Updates in the BE and Docs

--- webapp/src/handlers/sessionManager.js
+++ webapp/src/handlers/sessionManager.js
@@ -1,32 +1,15 @@
-import {useSession} from "@inrupt/solid-ui-react";
-import {checkForLomap} from "./podHandler";
-import * as sessionMetho from "@inrupt/solid-client-authn-browser";
+//import {useSession} from "@inrupt/solid-ui-react";
+//import * as sessionMetho from "@inrupt/solid-client-authn-browser";
 import {User} from "../models/user";
 import {readLocations, writeLocations} from "./podAccess";
 class SessionManager{
     //With this we can control the login status for solid
     //session;
-    session = sessionMetho;
-    isLoggedIn = false;
-    user = new User();
-    constructor() {
-        //this.session = useSession();
-        this.session.getDefaultSession()
-        //this.session = sessionMetho.getDefaultSession();
-    }
 
-    sessionState() {
-        //We have logged in
-        this.session.onLogin(async () =>
-        {
-            let pod = await checkForLomap(this.session);
-            this.user.setPodURL(pod);
-            this.isLoggedIn = true;
-        });
-        //We have logged out
-        this.session.onLogout(() => {
-            this.isLoggedIn = false;
-        })
+    session;
+    user = new User();
+    constructor(session) {
+        this.session = session;
     }
 
     async setUpSessionData(){
